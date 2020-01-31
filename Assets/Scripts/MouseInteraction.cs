bf556800ddf936ef00cf37f575cd445b386ef861
Assuroidi <35914867+Assuroidi@users.noreply.github.com>
2020-02-01 00:42:50 +0200

    Mouse clicks
    Implemented different kinds of mouse clicks. Add required methods from other scripts in inspector to this component.

---
+++ Assets/Scripts/MouseInteraction.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseInteraction : MonoBehaviour
+{
+    public float longClickTime;
+    [SerializeField]
+    private bool mousePressed;
+    [SerializeField]
+    private float timeClicked;
+    public UnityEvent shortClickEvents;
+    public UnityEvent longClickEvents;
+    public UnityEvent instantClickEvents;
+
+    private void Start()
+    {
+        if (longClickTime <= 0f)
+        {
+            longClickTime = 2f;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        instantClickEvents?.Invoke();
+        if (!mousePressed)
+        {
+            mousePressed = true;
+            StartCoroutine(MouseDownCounter());
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        mousePressed = false;
+    }
+
+    IEnumerator MouseDownCounter()
+    {
+        timeClicked = 0f;
+        while (mousePressed)
+        {
+            timeClicked += Time.deltaTime;
+            yield return null;
+        }
+        if (timeClicked < longClickTime)
+        {
+            shortClickEvents?.Invoke();
+        }
+        else
+        {
+            longClickEvents?.Invoke();
+        }
+    }
+
+    public void TestFunction1()
+    {
+        Debug.Log("Testing function 1");
+    }
+    public void TestFunction2()
+    {
+        Debug.Log("Testing function 2");
+    }
+    public void TestFunction3()
+    {
+        Debug.Log("Testing function 3");
+    }
+
+    //Kätevä, kannattaa kopioida
+#if UNITY_EDITOR
+    //using UnityEditor;
+
+    [UnityEditor.CustomEditor(typeof(MouseInteraction))]
+    public class MouseInteractionEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Test wut"))
+            {
+                Debug.Log("Wat");
+            }
+        }
+    }
+#endif
+
+}
