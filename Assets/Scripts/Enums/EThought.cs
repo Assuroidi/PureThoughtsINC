2bca89002d120adc7e4b596b56f4df9f508f78b6
Timo Ahtikari <timo.ahtikari@eficode.com>
2020-02-01 16:16:04 +0200

    Thought Control now works only with a sponge

---
+++ Assets/Scripts/Enums/EThought.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EThought
+{
+    Error = 0,  // Facilitates bug hunting
+    Sex = 1,
+    Drugs = 2,
+    Rock = 3,
+    Roll = 4,
+}
