290e70a18da0adcf66092519d9d374836cf5468c
Timo Seitz <tpseitz@mbnet.fi>
2020-02-02 01:10:31 +0200

    A beginning of start of first animation

---
+++ Assets/Scripts/TrayController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrayController : MonoBehaviour {
+  private Animator animator = null;
+
+  void Start() {
+    animator = GetComponent<Animator>();
+    animator.SetTrigger("open_tray");
+  }
+
+  void Update() {
+  }
+}
