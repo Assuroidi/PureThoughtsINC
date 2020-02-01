884fde07089165c8b1fcf9c45bdbac439f2970e3
Assuroidi <35914867+Assuroidi@users.noreply.github.com>
2020-02-01 14:04:00 +0200

    Toolstuff
    Some scripts are still work in progress

---
+++ Assets/Scripts/ToolHolder.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Click this to select tool.
+/// </summary>
+
+public class ToolHolder : MonoBehaviour
+{
+    public ETool originalTool;
+    [SerializeField]
+    private ETool currentTool;
+    private SpriteRenderer rend;
+    public Color HasToolColor;
+    public Color EmptyColor;
+    //public GameObject gameManager;
+    public ToolSelection gmToolScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTool = originalTool;
+        rend = gameObject.GetComponent<SpriteRenderer>();
+        gmToolScript = GameObject.FindGameObjectWithTag("GameManager")?.GetComponent<ToolSelection>(); ;
+        if (gmToolScript)
+        {
+            gmToolScript.AnnounceToolChanged += GmToolScript_AnnounceToolChanged;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (gmToolScript)
+        {
+            gmToolScript.AnnounceToolChanged -= GmToolScript_AnnounceToolChanged;
+        }
+    }
+
+    /// <summary>
+    /// This should reset the tool, when a different tool is selected while the original tool is already selected (thus current tool being none).
+    /// </summary>
+    /// <param name="toolEnum"></param>
+
+    private void GmToolScript_AnnounceToolChanged(ETool toolEnum)
+    {
+        if (currentTool == ETool.None && toolEnum != originalTool)
+        {
+            ToolHelper(originalTool, HasToolColor);
+        }
+    }
+
+    public void ToggleTool()
+    {
+        if (currentTool == ETool.None)
+        {
+            ToolHelper(originalTool, HasToolColor);
+        }
+        else
+        {
+            ToolHelper(ETool.None, EmptyColor);
+        }
+    }
+
+    private void ToolHelper(ETool tool, Color color)
+    {
+        currentTool = tool;
+        rend.color = color;
+    }
+}
---
+++ Assets/Scripts/ToolSelection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Replaces mouse sprite with selected tool.
+/// </summary>
+
+public class ToolSelection : MonoBehaviour
+{
+    public ETool selectedTool;
+    public delegate void ToolSelectionEvent(ETool toolEnum);
+    public event ToolSelectionEvent AnnounceToolChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectTool(ETool.None);
+    }
+
+    public void SelectTool(ETool tool)
+    {
+        if (tool == ETool.Error)
+        {
+            Debug.LogWarning("Invalid tool selection!");
+            return;
+        }
+        selectedTool = tool;
+        AnnounceToolChanged?.Invoke(tool);
+    }
+
+    
+
+}
