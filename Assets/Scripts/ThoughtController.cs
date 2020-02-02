f459047b05b542eb22ea43bedbee576be9f0a447
Timo Ahtikari <timo.ahtikari@eficode.com>
2020-02-02 13:47:13 +0200

    tuned the thoughtcontroller scrubbing function

--- Assets/Scripts/ThoughtController.cs
+++ Assets/Scripts/ThoughtController.cs
@@ -8,13 +8,15 @@ public class ThoughtController : MonoBehaviour
     public Vector3 pos;
     public float velx;
     public float vely;
-    public int health;
+    public float health;
     BoxCollider2D col2D;
     private ToolSelection gmToolScript;
     private bool thoughtControlEnabled;
     public Vector2 screenBounds;
     public Vector2 screenOrigo;
     public EThought type;
+    public bool beingScrubbed;
+    public int slowdownMultiplier;
     // Start is called before the first frame update
     void Start()
     {
@@ -56,7 +58,13 @@ public class ThoughtController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(velx*Time.deltaTime, vely*Time.deltaTime, 0);
+        if (beingScrubbed){
+            slowdownMultiplier = 10;
+            health -= 5*Time.deltaTime;
+        }else{
+            slowdownMultiplier = 1;
+        }
+        transform.Translate(velx/slowdownMultiplier*Time.deltaTime, vely/slowdownMultiplier*Time.deltaTime, 0);
         pos = transform.position;
         if (pos.x + col2D.size.x/2 > screenBounds.x || pos.x - col2D.size.x/2< screenOrigo.x)
         {
@@ -66,23 +74,19 @@ public class ThoughtController : MonoBehaviour
         {
             vely *= -1;
         }
+        if (health < 0){
+            Destroy(gameObject);
+        }
     }
 
     public void OnClick(){
         if (thoughtControlEnabled){
-            velx /= 10;
-            vely /= 10;
-            health -= 1;
-            if (health < 0){
-                Destroy(gameObject);
-            }
+            beingScrubbed = true;
         }
+        beingScrubbed = false;
     }
     public void OnRelease(){
-        if (thoughtControlEnabled){
-            velx *= 10;
-            vely *= 10;
-        }
+        beingScrubbed = false;
     }
 
     private void SetThoughSprite(GameObject item, EThought thought){
