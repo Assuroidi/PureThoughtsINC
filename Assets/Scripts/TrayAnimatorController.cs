84b072890d8862d383d5c7f0900b6464b82b2028
Ilse Heino <deathbringer.shoot44@hotmail.com>
2020-02-02 12:28:57 +0200

    Animator

---
+++ Assets/Scripts/TrayAnimatorController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrayAnimatorController : MonoBehaviour
+{
+    public Animator anim;
+    public bool trayOpen;
+    public bool nextBrains;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        trayOpen = false;
+        nextBrains = Input.GetMouseButton(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Pressed left click.");
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Pressed right click.");
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            Debug.Log("Pressed middle click.");
+        }
+        */
+        
+        if (nextBrains) //next brains button is pressed
+        {
+            anim.SetTrigger("Tray_auki");
+            Debug.Log("Pressed next brains.");
+            trayOpen = true;
+        }
+        else if (trayOpen = true && nextBrains) //and next brains button is pressed
+        {
+            anim.SetTrigger("Tray_kii");
+        }
+        else
+        {
+            anim.SetBool("Next_Brains", nextBrains);
+        }
+
+        /*
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.CompareTag("Enemy"))
+            {
+                animator.SetTrigger("Die");
+            }
+        }
+        */
+    }
+}
