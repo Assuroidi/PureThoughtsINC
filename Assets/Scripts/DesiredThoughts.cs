5df9e80cff18e28139eee3580eb8229fd74b54d1
Timo Ahtikari <timo.ahtikari@eficode.com>
2020-02-02 10:52:28 +0200

    DesiredThoughts should now change sprites based on assigned thought type. When returning orders, counting points should work if budgetmanager is present in the scene.

--- Assets/Scripts/DesiredThoughts.cs
+++ Assets/Scripts/DesiredThoughts.cs
@@ -9,28 +9,45 @@ public class DesiredThoughts : MonoBehaviour
     public List<EThought> requiredThoughts;
     public int lol;
     public GameObject[] thoughts;
+    public GameObject gm;
     void Start(){
         int thoughtCount = UnityEngine.Random.Range(1,3);
         int i = 0;
         while (i < thoughtCount){
-            requiredThoughts.Add((EThought)UnityEngine.Random.Range(1, Enum.GetValues(typeof(EThought)).Length));
+            EThought thought = (EThought)UnityEngine.Random.Range(1, Enum.GetValues(typeof(EThought)).Length);
+            requiredThoughts.Add(thought);
+            SetDesiredThoughSprite(transform.FindChild("item"+i).gameObject, thought);
             i++;
         }
+        gm = GameObject.FindWithTag("GameManager");
     }
 
     public void CompleteOrder(){
         thoughts = GameObject.FindGameObjectsWithTag("thought");
-        foreach (GameObject thought in thoughts){
-            CheckDesiredThoughtMatch(GetComponent<ThoughtController>().type);
+        if (thoughts.Length == 0)
+        {
+            Debug.Log("No game objects are tagged with 'thought'");
+        }
+        else
+        {
+            foreach (GameObject thought in thoughts){
+                CheckDesiredThoughtMatch(thought.GetComponent<ThoughtController>().type);
+            }
         }
     }
 
     private void CheckDesiredThoughtMatch(EThought thought){
         if(requiredThoughts.Contains(thought)){
-            BudgetManager.AddFunds(100);
+            gm.GetComponent<BudgetManager>().AddFunds(100);
+            Debug.Log("löyty!");
         }
         else{
-            BudgetManager.AddFunds(-200);
+            gm.GetComponent<BudgetManager>().AddFunds(-200);
+            Debug.Log("ei löytyny!");
         }
+    }
+    private void SetDesiredThoughSprite(GameObject child, EThought thought){
+        Sprite thoughtSprite = Resources.Load(Enum.GetName(thought), typeof(Sprite)) as Sprite;
+        child.GetComponent(SpriteRenderer).sprite = thoughtSprite;
     }
 }
